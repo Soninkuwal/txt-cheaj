9f0619565db52e0da077ff85529ce8bdfc08dc40
Soninkuwal <158008154+Soninkuwal@users.noreply.github.com>
2024-11-23 10:48:45 +0530

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -1,6 +1,6 @@
 # config.py
-BOT_NAME = ""
-BOT_TOKEN = ""
-API_ID = ""
-API_HASH = ""
+BOT_NAME = os.environ.get("BOT_NAME", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+API_ID = os.environ.get("API_ID", "")
+API_HASH = os.environ.get("API_HASH", "")
 MONGO_URI = "mongodb+srv://mrnobody:modernhackers@mrnobody.q8e87ij.mongodb.net/?retryWrites=true&w=majority&appName=MrNobody"
