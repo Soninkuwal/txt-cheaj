842dddecb9ec17b7bc77f2115db70cd2fe51b999
Soninkuwal <158008154+Soninkuwal@users.noreply.github.com>
2024-11-23 11:01:35 +0530

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -1,6 +1,6 @@
 # config.py
 BOT_NAME = "TXTKANHA1BOT"
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = os.environ.get("API_ID", "")
-API_HASH = os.environ.get("API_HASH", "")
+BOT_TOKEN = int(os.environ.get("BOT_TOKEN", ""))
+API_ID = int(os.environ.get("API_ID", ""))
+API_HASH = int(os.environ.get("API_HASH", ""))
 MONGO_URI = "mongodb+srv://mrnobody:modernhackers@mrnobody.q8e87ij.mongodb.net/?retryWrites=true&w=majority&appName=MrNobody"
